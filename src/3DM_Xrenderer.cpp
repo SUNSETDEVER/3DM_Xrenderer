e1d8ee68932f4dfaf81d5467490090792ef51cd4
SUNSET_STEC <973879601@qq.com>
2026-08-17 18:15:07 +0800

    Update 3DM_Xrenderer.cpp

--- src/3DM_Xrenderer.cpp
+++ src/3DM_Xrenderer.cpp
@@ -1,5 +1,5 @@
 ﻿/**
- * @file 3DM_Xrenderer.c
+ * @file 3DM_Xrenderer.cpp
  * @brief Implementation of 3DM_Xrenderer library.
  * @version 1.3  (Added mesh texture support with UV interpolation)
  */
